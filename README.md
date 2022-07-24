ea458d445fd0f5e6800d3434df57fdf648dcf999
spopp20 <stephen.popp20@gmail.com>
2022-07-24 01:09:53 -0400

    Added a seed script to populate the database
    More testing needs to be done currently.

--- README.md
+++ README.md
@@ -33,3 +33,10 @@ After getting the source code the next steps are:
 ### npm run build - do a production build
 ### npm run start - run the application in production mode
 ### npm run lint - lint the code
+## Prisma Commands
+
+### npx prisma format - formats the prisma schema file
+### npx prisma migrate dev --name init - changes the database schema and seeds the database
+### npx prisma studio - runs a web app to show database data
+
+
---
+++ prisma/seed.ts
@@ -0,0 +1,77 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const userData: Prisma.UserCreateInput[] = [
+    {
+        name: 'Alice',
+        email: 'alice@concert.io',
+    },
+    {
+        name: 'Art',
+        email: 'art@concert.io',
+    },
+    {
+        name: 'John',
+        email: 'john@concert.io',
+    },
+]
+
+const eventData: Prisma.EventCreateInput[] = [
+    {
+        name: 'Morning Event',
+        description: 'This is a sample event.',
+        active: 1,
+    },
+    {
+        name: 'Evening Event',
+        description: 'This is a very long event.',
+        active: 1,
+    }
+]
+
+const instrumentData: Prisma.InstrumentCreateInput[] = [
+    {
+        name: 'Piano',
+        description: 'Concert Piano',
+        active: 1,
+    },
+    {
+        name: 'Guitar',
+        description: 'Electric Guitar',
+        active: 1,
+    },
+]
+
+async function main() {
+    console.log(`Start seeding ...`)
+    for (const u of userData) {
+        const user = await prisma.user.create({
+            data: u,
+        })
+        console.log(`Created user with id: ${user.id}`)
+    }
+    for (const u of eventData) {
+        const event = await prisma.event.create({
+            data: u,
+        })
+        console.log(`Created event with id: ${event.id}`)
+    }
+    for (const u of instrumentData) {
+        const instrument = await prisma.instrument.create({
+            data: u,
+        })
+        console.log(`Created instrument with id: ${instrument.id}`)
+    }
+    console.log(`Seeding finished.`)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
